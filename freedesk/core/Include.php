a4b56e22ca983b1626caa7acd7ba928bfce296f2
David Cutting <dcutting@purplepixie.org>
2012-05-21 15:43:57 +0100

    Include and FreeDESK classes

---
+++ freedesk/core/Include.php
@@ -0,0 +1,50 @@
+<?php 
+/* -------------------------------------------------------------
+This file is part of FreeDESK
+
+FreeDESK is (C) Copyright 2012 David Cutting
+
+FreeDESK is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FreeDESK is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FreeDESK.  If not, see www.gnu.org/licenses
+
+For more information see www.purplepixie.org/freedesk/
+-------------------------------------------------------------- */
+
+/**
+ * Include class handles most php source file includes at runtime
+**/
+
+class Include
+{
+	/**
+	 * Reference to FreeDESK class
+	**/
+	private var $DESK = null;
+	/**
+	 * Base directory
+	**/
+	private var $baseDir = "./";
+	
+	/**
+	 * Constructor
+	 * @param object $freeDESK freedesk main object
+	 * @param string $baseDir base directory
+	**/
+	function Include(&$freeDESK, $baseDir)
+	{
+		$this->DESK = freeDESK;
+		$this->baseDir = $baseDir;
+	}
+
+
+?>
