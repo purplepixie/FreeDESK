fc9ea953b559c3b3e997856f13eb0d89d0af4655
David Cutting <dcutting@purplepixie.org>
2012-07-03 01:43:33 +0100

    Customer NYI index

---
+++ freedesk/customer/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--
+/* -------------------------------------------------------------
+This file is part of FreeDESK
+
+FreeDESK is (C) Copyright 2012 David Cutting
+
+FreeDESK is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FreeDESK is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FreeDESK.  If not, see www.gnu.org/licenses
+
+For more information see www.purplepixie.org/freedesk/
+-------------------------------------------------------------- */
+-->
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../skin/default/css/main.css" />
+<title>Not Yet Implemented...</title>
+</head>
+<body>
+<div>
+<h2>Sorry Customer Interface Not Yet Implemented</h2>
+
+It is however due for implementation soon. Keep an eye on the <a href="http://freedesk.purplepixie.org/">main site</a> and the <a href="http://blog.freedesk.purplepixie.org/">blog</a>.
+<br /><br />
+
+<a href="../">Go back to the main interface</a><br /><br />
+</div>
+</body>
+</html>
+
